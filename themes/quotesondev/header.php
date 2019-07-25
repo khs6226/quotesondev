65ed24f00ecbe855885fe7a0915c1ee28343a9cf
khs6226 <khsun6226@naver.com>
2019-07-24 20:08:09 -0700

    add logo to header

--- themes/quotesondev/header.php
+++ themes/quotesondev/header.php
@@ -17,19 +17,17 @@
 	</head>
 
 	<body <?php body_class(); ?>>
-		<div id="page" class="hfeed site">
+		<div id="page" class="hfeed site container">
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
+					<div class="logo">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+					<a href="localhost/quotesondev">
+						<img src="wp-content/themes/quotesondev/assets/qod-logo.svg" alt="Quotes on Dev logo">
+					</a>
 				</div><!-- .site-branding -->
-
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
