92b931c7290650c7eadaf515385dc8a542238f5b
Hongsoon Kim <khsun6226@naver.com>
2019-08-29 04:04:49 -0700

    add submit function.

--- themes/quotesondev/js/main.js
+++ themes/quotesondev/js/main.js
@@ -1,7 +1,8 @@
 jQuery(($) => {
     const $quote_btn = $('#quote-button'),
           $content = $('.entry-content'),
-          $title = $('.entry-meta');
+          $title = $('.entry-meta'),
+          $submit = $('.submit-button');
     
     window.addEventListener('popstate', function() {
         window.location=window.location;
@@ -27,5 +28,52 @@ jQuery(($) => {
                         ${answer._qod_quote_source}</a>
                     </span>`);      
         });
+    });
+
+    $submit.on('click', function(event) {
+        event.preventDefault();
+
+        let $author= $('.author').val(),
+              $quote= $('.content').val(),
+              $source= $('.source').val(),
+              $url= $('.url').val();
+
+        if ($author === '') {
+            alert('Please provide the author of the quote.');
+            $('.author').focus();
+            
+        } else if ($quote === '') {
+            alert('Please provide the quote.');
+            $('.content').focus();
+         
+        } else if ($source === '') {
+            alert('Please provide the source of the quote.');
+            $('.source').focus();
+            
+        } else {
+            $.ajax({
+                method: 'POST',
+                url: qod_vars.rest_url + 'wp/v2/posts/',
+                data: {
+                    title : $author,
+                    content : $quote,
+                    quote_source : $source,
+                    quote_url : $url
+                },
+
+                beforesend: function(xhr) {
+                    xhr.setRequestHeader('X-WP-Nonce', qod_vars.wpapi_nonce);
+                }
+            }).done(function() {
+                alert('Thank you for submitting your quote!');
+                $author = '';
+                $source = '';
+                $quote = '';
+                $url = '';
+                $('#submit-form').submit();
+            }).fail(function() {
+                alert('Please try again.');
+            });
+        }
     });
 });
--- themes/quotesondev/template-parts/content-submit.php
+++ themes/quotesondev/template-parts/content-submit.php
@@ -9,22 +9,22 @@
 <form action="" id="submit-form">
     <label for="author">
         <p class="submit_p">Author of Quote</p>
-        <input type="text" name="author" id="author">
+        <input type="text" name="author" class="author">
     </label>
 
     <label for="content">
         <p class="submit_p">Quote</p>
-        <textarea name="content" id="content"></textarea>
+        <textarea name="content" class="content"></textarea>
     </label>
 
     <label for="source">
         <p class="submit_p">Where did you find this quote? (e.g. book name)</p>
-        <input type="text" name="source" id="source">
+        <input type="text" name="source" class="source">
     </label>
 
     <label for="url">
         <p class="submit_p">Provide the url of the quote source, if available</p>
-        <input type="url" name="url" id="url">
+        <input type="url" name="url" class="url">
     </label>
 
     <p class="submit_p"><button class="submit-button" type="submit">Submit Quote</button></p>
