14af9c754a87f03adcf5b59ee8cb322fcc331376
Hongsoon Kim <khsun6226@naver.com>
2019-08-29 02:47:56 -0700

    design 404.php

--- themes/quotesondev/searchform.php
+++ themes/quotesondev/searchform.php
@@ -3,8 +3,10 @@
 		<label>
 			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
-		<button class="search-submit">
-			<?php echo esc_html( 'Search' ); ?>
-		</button>
+		<a class="search-submit" href='<?php echo esc_html(''); ?>'>
+			<span class='search-icon'>
+				<i class="fa fa-search"></i>
+			</span>
+		</a>
 	</fieldset>
 </form>
